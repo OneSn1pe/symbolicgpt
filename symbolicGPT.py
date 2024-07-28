21ad5e76318bb84f1742616bc43c9d6a4930d20c
Kaustubh <84277806+OneSn1pe@users.noreply.github.com>
2024-07-28 01:03:24 -0400

    Update symbolicGPT.py
    change train_file value to local file

--- symbolicGPT.py
+++ symbolicGPT.py
@@ -75,7 +75,7 @@ except:
     print('Folder already exists!')
 
 # load the train dataset
-train_file = 'train_dataset_{}.pb'.format(fName)
+train_file = '/Users/kaustubhkislay/Downloads/SymbolicGPT/Datasets/1Var_RandSupport_FixedLength_-3to3_-5.0to-3.0-3.0to5.0_30Points/Train/0_1_0_14062021_193012.json'
 if os.path.isfile(train_file) and not scratch:
     # just load the train set
     with open(train_file, 'rb') as f:
